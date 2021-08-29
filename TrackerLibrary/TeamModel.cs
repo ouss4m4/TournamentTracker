b3e53de30b4a23d5c3f7fda327c77553ac73726c
oussama <bz.oussama@gmail.com>
2021-08-29 19:05:45 +0100

    Add project files.

---
+++ TrackerLibrary/TeamModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TrackerLibrary
+{
+    public class TeamModel
+    {
+        public List<PersonModel> TeamMembers { get; set; } = new List<PersonModel>();
+        public string TeamName { get; set; }
+
+
+    }
+}
