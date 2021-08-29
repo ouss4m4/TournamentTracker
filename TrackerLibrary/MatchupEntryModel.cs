4198a8e1d2e92e27773ceb0bc228ed470fe1b8d3
oussama <bz.oussama@gmail.com>
2021-08-29 19:24:52 +0100

    renamed to match convention

--- TrackerLibrary/MatchupEntryModel.cs
+++ TrackerLibrary/MatchupEntryModel.cs
@@ -8,8 +8,19 @@ namespace TrackerLibrary
 {
     public class MatchupEntryModel
     {
+        /// <summary>
+        /// Represents one team in the matchup
+        /// </summary>
         public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
+        /// <summary>
+        /// Represents the score for this particular team
+        /// </summary>
         public TeamModel Winner { get; set; }
+        /// <summary>
+        /// Represents the matchup tha this team came 
+        /// from as the winner.
+        /// </summary>
         public int MatchupRound { get; set; }
+
     }
 }
